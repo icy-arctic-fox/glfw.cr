e98ec88196c37161765f53c26058d757bb1fdd51
Michael Miller <icy.arctic.fox@gmail.com>
2019-06-03 22:51:55 -0600

    Create enum for init hint

--- src/glfw/init.cr
+++ src/glfw/init.cr
@@ -1,18 +1,12 @@
 @[Link("glfw3")]
 lib LibGLFW
-  JOYSTICK_HAT_BUTTONS = 0x00050001
-
-  COCOA_CHDIR_RESOURCES = 0x00051001
-
-  COCOA_MENUBAR = 0x00051002
-
   alias ErrorProc = ErrorCode, UInt8* -> Void
 
   fun init = glfwInit : Int32
 
   fun terminate = glfwTerminate : Void
 
-  fun init_hint = glfwInitHint(hint : Int32, value : Int32) : Void
+  fun init_hint = glfwInitHint(hint : InitHint, value : Bool) : Void
 
   fun get_version = glfwGetVersion(major : Int32*, minor : Int32*, rev : Int32*) : Void
 
---
+++ src/glfw/init_hint.cr
@@ -0,0 +1,8 @@
+@[Link("glfw3")]
+lib LibGLFW
+  enum InitHint : Int32
+    JoystickHatButtons = 0x00050001
+    CocoaChdirResources = 0x00051001
+    CocoaMenubar = 0x00051002
+  end
+end
