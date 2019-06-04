479a6003a25a836a62eb2189575e7fec5c3ff784
Michael Miller <icy.arctic.fox@gmail.com>
2019-06-04 12:23:15 -0600

    Add missing enum values

--- src/glfw/input/input_mode.cr
+++ src/glfw/input/input_mode.cr
@@ -1,9 +1,9 @@
 lib LibGLFW
   enum InputMode : Int32
-    Cursor
-    StickyKeys
-    StickyMouseButtons
-    LockKeyMods
-    RawMouseMotion
+    Cursor             = 0x00033001
+    StickyKeys         = 0x00033002
+    StickyMouseButtons = 0x00033003
+    LockKeyMods        = 0x00033004
+    RawMouseMotion     = 0x00033005
   end
 end
