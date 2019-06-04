f7b95fe34b233403437da6155f5f5f85c70ae54c
Michael Miller <icy.arctic.fox@gmail.com>
2019-06-04 12:23:20 -0600

    Formatting

--- src/glfw/init_hint.cr
+++ src/glfw/init_hint.cr
@@ -1,7 +1,7 @@
 lib LibGLFW
   enum InitHint : Int32
-    JoystickHatButtons = 0x00050001
+    JoystickHatButtons  = 0x00050001
     CocoaChdirResources = 0x00051001
-    CocoaMenubar = 0x00051002
+    CocoaMenubar        = 0x00051002
   end
 end
