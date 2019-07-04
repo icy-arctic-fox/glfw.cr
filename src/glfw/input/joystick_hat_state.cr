c1656f688adac46db8d1944dbb91e38fc11b2931
Michael Miller <icy.arctic.fox@gmail.com>
2019-07-04 17:58:35 -0600

    Remove combined enum values

--- src/glfw/input/joystick_hat_state.cr
+++ src/glfw/input/joystick_hat_state.cr
@@ -6,9 +6,5 @@ lib LibGLFW
     Right     = 2
     Down      = 4
     Left      = 8
-    RightUp   = Right | Up
-    RightDown = Right | Down
-    LeftUp    = Left | Up
-    LeftDown  = Left | Down
   end
 end
