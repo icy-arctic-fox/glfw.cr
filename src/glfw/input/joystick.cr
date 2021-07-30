9e551e6efe39d057498863d08f776ed4c7cbbde7
Michael Miller <icy.arctic.fox@gmail.com>
2021-07-29 23:21:22 -0600

    Formatting

--- src/glfw/input/joystick.cr
+++ src/glfw/input/joystick.cr
@@ -1,20 +1,20 @@
 lib LibGLFW
   enum Joystick : Int32
-    Joy1   =  0
-    Joy2   =  1
-    Joy3   =  2
-    Joy4   =  3
-    Joy5   =  4
-    Joy6   =  5
-    Joy7   =  6
-    Joy8   =  7
-    Joy9   =  8
-    Joy10  =  9
-    Joy11  = 10
-    Joy12  = 11
-    Joy13  = 12
-    Joy14  = 13
-    Joy15  = 14
-    Joy16  = 15
+    Joy1  =  0
+    Joy2  =  1
+    Joy3  =  2
+    Joy4  =  3
+    Joy5  =  4
+    Joy6  =  5
+    Joy7  =  6
+    Joy8  =  7
+    Joy9  =  8
+    Joy10 =  9
+    Joy11 = 10
+    Joy12 = 11
+    Joy13 = 12
+    Joy14 = 13
+    Joy15 = 14
+    Joy16 = 15
   end
 end
--- src/glfw/input/joystick_hat_state.cr
+++ src/glfw/input/joystick_hat_state.cr
@@ -1,10 +1,10 @@
 lib LibGLFW
   @[Flags]
   enum JoystickHatState : UInt8
-    Centered  = 0
-    Up        = 1
-    Right     = 2
-    Down      = 4
-    Left      = 8
+    Centered = 0
+    Up       = 1
+    Right    = 2
+    Down     = 4
+    Left     = 8
   end
 end
