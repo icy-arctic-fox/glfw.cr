642a64939a875d4117e5e928b2d1e3b05bcfc070
Michael Miller <icy.arctic.fox@gmail.com>
2022-01-27 20:49:13 -0700

    Add bool conversion methods

--- src/glfw/bool.cr
+++ src/glfw/bool.cr
@@ -2,5 +2,16 @@ lib LibGLFW
   enum Bool : Int32
     False = 0
     True  = 1
+
+    # Converts a value to a GLFW boolean.
+    # Returns `True` if value is truthy, `False` otherwise.
+    def self.new(value)
+      value ? True : False
+    end
+
+    # Converts to a native Crystal `Bool`.
+    def to_bool
+      !false?
+    end
   end
 end
